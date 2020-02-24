a6cf60ca69fcbdf91abc486d9ce16455a72a0a33
Tomasch <tomasch3034@web.de>
2020-02-24 17:45:34 +0100

    reformat code

--- src/test/java/jenkins/task/TestUtil/FakeTaskListener.java
+++ src/test/java/jenkins/task/TestUtil/FakeTaskListener.java
@@ -12,43 +12,60 @@ import java.io.PrintWriter;
 import java.nio.charset.Charset;
 
 public class FakeTaskListener implements TaskListener {
-    @Nonnull @Override public PrintStream getLogger() {
+    @Nonnull
+    @Override
+    public PrintStream getLogger() {
         return null;
     }
 
-    @Nonnull @Override public Charset getCharset() {
+    @Nonnull
+    @Override
+    public Charset getCharset() {
         return null;
     }
 
-    @Override public PrintWriter _error(String prefix, String msg) {
+    @Override
+    public PrintWriter _error(String prefix, String msg) {
         return null;
     }
 
-    @Override public void annotate(ConsoleNote ann) throws IOException {
+    @Override
+    public void annotate(ConsoleNote ann) throws IOException {
 
     }
 
-    @Override public void hyperlink(String url, String text) throws IOException {
+    @Override
+    public void hyperlink(String url, String text) throws IOException {
 
     }
 
-    @Nonnull @Override public PrintWriter error(String msg) {
+    @Nonnull
+    @Override
+    public PrintWriter error(String msg) {
         return null;
     }
 
-    @Nonnull @Override public PrintWriter error(String format, Object... args) {
+    @Nonnull
+    @Override
+    public PrintWriter error(String format, Object... args) {
         return null;
     }
 
-    @Nonnull @Override public PrintWriter fatalError(String msg) {
+    @Nonnull
+    @Override
+    public PrintWriter fatalError(String msg) {
         return null;
     }
 
-    @Nonnull @Override public PrintWriter fatalError(String format, Object... args) {
+    @Nonnull
+    @Override
+    public PrintWriter fatalError(String format, Object... args) {
         return null;
     }
 
-    @Nonnull @Override public Channel getChannelForSerialization() throws NotSerializableException {
+    @Nonnull
+    @Override
+    public Channel getChannelForSerialization() throws NotSerializableException {
         return null;
     }
 }
